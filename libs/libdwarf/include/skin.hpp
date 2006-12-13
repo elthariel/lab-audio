a7756cf8201d5834a6ae6b328070dc79945e4a6f
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2006-12-13 23:46:11 +0000

    Design du skin en place

--- libs/libdwarf/include/skin.hpp
+++ libs/libdwarf/include/skin.hpp
@@ -1,18 +1,23 @@
 #include <map>
-//#include <vector>
 #include <string>
 #include <gdkmm.h>
 
 using namespace Gdk;
+using namespace std;
 
+template <class ST>
 class SElement
 {
 	SElement();
-public:
-	SElement(Stype *, map<string, string>);
 
-	virtual Glib::RefPtr<Pixmap> render(float *a_states,
-										unsigned int a_state_count) = 0;
+  ST                    m_stype;
+  string                m_name;
+
+public:
+  SElement(string a_name);
+
+	virtual Glib::RefPtr<Pixbuf> render(float *a_states,
+                                      unsigned int a_state_count) = 0;
 };
 // <-- SERotary
 
@@ -20,32 +25,35 @@ public:
 class SType
 {
 	SType();
-	string						m_stype_name;
+	string                m_stype_name;
 
 public:
 	SType(string a_name);
 
+  const string          &get_name();
+  bool                  operator==(SType &);
+  bool                  operator==(string &);
 };
 
 
 typedef cmap<char *, SElement *> _SEMap;
 
-/** Represent the skin for a plugin. It allows you to register STypeX objects 
+/** Represent the skin for a plugin. It allows you to register STypeX objects
  *  at runtime. It provides access to skin elements to the ui.
  */
 class SInstance
 {
 	/** Skin elements map.
 	*/
-	_SEMap			m_emap;
-	 
+	_SEMap        m_emap;
+
 public:
-	SElement		*get_element(char *);
-	void			register_element(SElement *);
+	SElement      *get_element(char *);
+	void          register_element(SElement *);
 
 };
 
-/* Will be implemented in middle-term; Unique instance which provides access 
+/* Will be implemented in middle-term; Unique instance which provides access
  * to all the skins in the process.
  */
 class SManager
--- libs/libdwarf/include/stype_imgs.hpp
+++ libs/libdwarf/include/stype_imgs.hpp
@@ -1,10 +1,48 @@
+//
+// stype_imgs.hpp for libdwarf in /home/elthariel/code/projs/lab-audio/libs/libdwarf
+//
+// Made by Elthariel
+// Login   <elthariel@free.fr>
+//
+// Started on  Thu Dec 14 00:04:57 2006 Elthariel
+// Last update Thu Dec 14 00:45:49 2006 Elthariel
+//
+
 #include "skin.hpp"
+#include <vector>
 
 class STypeImgs : public SType
 {
 	int						m_state_count;
+
 public:
 	STypeImgs(string a_name, int a_state_count);
 
-	Glib::RefPtr<Pixmap>	make_pixmap(string a_img_base_path, unsigned int a_state);
+	Glib::RefPtr<Pixbuf>	make_pixmap(string a_img_base_path,
+                                    unsigned int a_state);
 };
+
+
+template <int p_state_count>
+class SEImgs : public SElement<STypeImgs>
+{
+  SEToggle();
+
+  vector<int, Glib::RefPtr<Pixbuf>> m_imgs;
+  string                m_base_path;
+
+  void                  build_imgs();
+
+public:
+  SEToggle(string a_element_name, string a_base_path);
+
+	virtual Glib::RefPtr<Pixbuf> render(float *a_states,
+                                      unsigned int a_state_count);
+};
+
+typedef SEImgs<2>       SEImgsToggle;
+typedef SEImgs<4>       SEImgsToggleAnim;
+typedef SEImgs<127>     SEImgsSlider;
+
+
+void                    append_int_to_string(string &str, unsigned int i);
