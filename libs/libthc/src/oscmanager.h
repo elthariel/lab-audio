4b94acd6c351fdebce5b18d27ee08d56ac640b07
goctaf <goctaf@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-01-02 21:10:28 +0000

    starting to support osc (nothing work)

--- libs/libthc/src/oscmanager.h
+++ libs/libthc/src/oscmanager.h
@@ -28,10 +28,32 @@
 #include <vector>
 #include <gtkmm.h>
 #include "ithcwidget.h"
-//#include "lo/lo.h"
+#include "lo/lo.h"
 
 namespace Thc {
   
+  /** /widget/widgetname/paramname/set_value
+   *  /widget/widgetname/paramname/get_value
+   *  /widget/widgetname/paramname/set_adj
+   *  /widget/widgetname/paramname/get_adj
+   *  /widget/widgetname/get_param_count
+   *  /widget/widgetname/get_param_name
+   *  /widget/drag
+   *  /widget/drop
+   */
+  
+  
+/* Some boilerplate killing macros... */
+  #define LO_HANDLER_ARGS const char* path, const char* types, lo_arg** argv, int argc, lo_message msg
+
+/* Defines a static handler to be passed to lo_add_method, which is a trivial
+ * wrapper around a non-static method that does the real work.  Makes a whoole
+ * lot of ugly boiler plate go away */
+  #define LO_HANDLER(name) \
+    int m_##name##_cb (LO_HANDLER_ARGS);\
+    inline static int name##_cb(LO_HANDLER_ARGS, void* myself)\
+    { return ((OSCEngineReceiver*)myself)->m_##name##_cb(path, types, argv, argc, msg); }
+
   class OscManager {
   public:
     typedef std::vector<IThcWidget *> Vector;
@@ -39,10 +61,17 @@ namespace Thc {
   
     inline static void instanciate() { if (!m_osc_manager) m_osc_manager = new OscManager(); }
     inline static OscManager *instance() { return m_osc_manager; }
+    
   protected:
     OscManager() {};
-    
+    static void error_cb(int num, const char* msg, const char* path);
+	  static int  set_response_address_cb(LO_HANDLER_ARGS, void* myself);
+	  static int  generic_cb(LO_HANDLER_ARGS, void* myself);
+	  static int  unknown_cb(LO_HANDLER_ARGS, void* myself);
+	    
   private:
+  	Glib::ustring m_port;
+	  lo_server m_server;
     static OscManager *m_osc_manager;
     //list
   };
