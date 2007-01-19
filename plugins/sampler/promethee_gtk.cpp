552a37189607581ed72d5cc88bf554c022ffa41c
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-01-19 08:00:24 +0000

    Started to build sampler gui

--- plugins/sampler/promethee_gtk.cpp
+++ plugins/sampler/promethee_gtk.cpp
@@ -5,96 +5,76 @@
 // Login   <elthariel@lse.epita.fr>
 //
 // Started on  Thu Jan 18 16:58:36 2007 Elthariel
-// Last update Thu Jan 18 22:25:18 2007 Nahlwe
+// Last update Fri Jan 19 08:52:09 2007 Nahlwe
 //
 
 #include <iostream>
-
-#include <gtkmm.h>
-
-#include "lv2gtk2gui.hpp"
-#include "promethee.peg"
-
+#include "promethee_gtk.hh"
 
 using namespace std;
 using namespace Gtk;
 using namespace sigc;
 
-class PrometheeGUI : public LV2GTK2GUI
+/*
+ * SampleEdit class.
+ */
+
+SampleEdit::SampleEdit(LV2Controller& ctrl)
+  // m_main_adj(0.5, 0.0, 1.0, 0.05)
+    //    m_main_scale(m_main_adj[0])
+  : m_ctrl(ctrl),
+    m_main_adj_vol(1.0, 0.0, 2.0, 0.01),
+    m_main_adj_pan(1.0, 0.0, 2.0, 0.01),
+    m_main_adj_pitch(1.0, 0.0, 2.0, 0.01),
+    m_main_adj_root(63.0, 0.0, 127.0, 1.0),
+    m_main_scale_vol(m_main_adj_vol),
+    m_main_scale_pan(m_main_adj_pan),
+    m_main_scale_pitch(m_main_adj_pitch),
+    m_main_scale_root(m_main_adj_root)
 {
-public:
-  PrometheeGUI(LV2Controller& ctrl,
-               const std::string& URI,
-               const std::string& bundle_path,
-               Widget*& widget)
-    : m_button("Trigger sample"), m_ctrl(ctrl)
-  {
+  int i;
 
-    Gtk::FileChooserDialog dialog("Choose a sample",
-                                  Gtk::FILE_CHOOSER_ACTION_OPEN);
-    //    dialog.set_transient_for(widget);
+  for (i = 0; i < sedit_n_hbox; i++)
+    this->pack_start(m_vbox[i]);
 
-    //Add response buttons the the dialog:
-    dialog.add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
-    dialog.add_button(Gtk::Stock::OPEN, Gtk::RESPONSE_OK);
+  m_lbl_main[0].set_label("Vol");
+  m_lbl_main[1].set_label("Pan");
+  m_lbl_main[2].set_label("Pitch");
+  m_lbl_main[3].set_label("Root");
+  m_vbox_main[0].pack_start(m_main_scale_vol);
+  m_vbox_main[1].pack_start(m_main_scale_pan);
+  m_vbox_main[2].pack_start(m_main_scale_pitch);
+  m_vbox_main[3].pack_start(m_main_scale_root);
+  for (i = 0; i < 4; i++)
+    {
+      m_vbox_main[i].pack_start(m_lbl_main[i]);
+      m_vbox[sedit_main].pack_start(m_vbox_main[i]);
+    }
+}
 
-    Gtk::FileFilter filter_samples;
-    filter_samples.set_name("Audio sample Files");
-    filter_samples.add_pattern("*.wav");
-    filter_samples.add_pattern("*.aiff");
-    dialog.add_filter(filter_samples);
+/*
+ * PrometheeGUI class.
+ */
 
-    int result = dialog.run();
-    dialog.hide();
-    //Handle the response:
-    switch(result)
-      {
-      case(Gtk::RESPONSE_OK):
-        {
-          std::cout << "Open clicked." << std::endl;
-          std::string filename = dialog.get_filename();
-          std::cout << "File selected: " <<  filename << std::endl;
-          m_ctrl.set_file("sample", filename.c_str());
-          break;
-        }
-      case(Gtk::RESPONSE_CANCEL):
-        {
-          std::cout << "Cancel clicked." << std::endl;
-          break;
-        }
-      default:
-        {
-          std::cout << "Unexpected button clicked." << std::endl;
-          break;
-        }
-      }
-    widget = &m_button;
+PrometheeGUI::PrometheeGUI(LV2Controller& ctrl,
+                           const std::string& URI,
+                           const std::string& bundle_path,
+                           Widget*& widget)
+  :m_ctrl(ctrl)
+{
+  int i;
 
-    m_button.signal_pressed().connect(bind(mem_fun(*this, &PrometheeGUI::button_clik), 0));
-    m_button.signal_released().connect(bind(mem_fun(*this, &PrometheeGUI::button_clik), 1));
-  }
+  widget = &m_smp_notebook;
+  m_smp_notebook.set_tab_pos(POS_LEFT);
 
-  void                  button_clik(int a_mode)
-  {
-    unsigned char _on[3] = { 0x90, 1, 127 };
-    unsigned char _off[3] = { 0x80, 1, 0 };
-
-    if (a_mode == 0)
-      {
-        cout << "trigger on" << endl;
-        m_ctrl.send_midi(peg_midi, 3, _on);
-      }
-    else
-      {
-        cout << "trigger off" << endl;
-        m_ctrl.send_midi(peg_midi, 3, _off);
-      }
-  }
-protected:
-  Button                m_button;
-  LV2Controller			&m_ctrl;
-
-};
+  for (i = 0; i < SAMPLES_COUNT; i++)
+    {
+      m_smp_notebook.append_page(m_smp_edit[i], "_fRe3_");
+    }
+  /*  m_button.signal_released().connect(bind(mem_fun(*this,
+                                                  &PrometheeGUI::button_clik), 1));
+  */
+}
 
 void initialise() __attribute__((constructor));
 void initialise() {
