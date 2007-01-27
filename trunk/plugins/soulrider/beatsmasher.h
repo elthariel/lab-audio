5cf00b2ef48382125be76209473f076765e1be81
ctaf42 <ctaf42@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-01-27 02:38:08 +0000

    beatsmasher

---
+++ trunk/plugins/soulrider/beatsmasher.h
@@ -0,0 +1,64 @@
+	/****************************************************************************
+
+    myplugin.cpp - example file for writing LV2 plugins in C++
+
+    Copyright (C) 2006  Lars Luthman <lars.luthman@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
+
+****************************************************************************/
+
+class BitSmasher {
+	public:
+		BitSmasher(int samplerate) {
+			m_samplerate = samplerate;
+			m_buffer = new float[samplerate];
+			m_active = false;
+		}
+		
+		void process(float *buffer, int samplecount) {
+			int i = 0, j = 0;
+			if (!m_active)
+				return;
+			if (m_loadbuffer) {
+				for (m_pos; m_pos < m_loop_size && j < samplecount; ++m_pos) {
+					m_buffer[m_pos] = buffer[j];
+					++j;
+				}
+			}
+			
+			
+			
+			
+			
+		}
+		
+		inline void active(bool active) { 
+			m_active = active;
+			if (m_active = false) {
+			}
+		}
+
+	private:
+		float *m_buffer;
+		int m_samplerate;
+		int m_loop_size;
+		float m_gate;
+		bool m_loadbuffer;
+		bool m_active;
+		bool m_sync;
+		
+};
+
