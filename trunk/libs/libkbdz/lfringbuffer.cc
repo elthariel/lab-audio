5d1442f2ea7b7ab87ee67c7055a0ddef204ca2ab
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-01-27 10:18:03 +0000

    LFRing & smp_uint

--- trunk/libs/libkbdz/lfringbuffer.cc
+++ trunk/libs/libkbdz/lfringbuffer.cc
@@ -26,7 +26,127 @@
 template <class BlockType>
 LFRingbuffer::LFRingbuffer(uint32_t item_count)
   : m_buffer(0), m_ro(0), m_wo(0), m_capacity(item_count),
-    m_occupation(0), m_reader(false), m_writer(false)
+    m_size(), m_reader(false), m_writer(false)
 {
   m_buffer = new BlockType[item_count];
+}
+
+template <class BlockType>
+LFRingBufferWriter<BlockType>   *LFRingbuffer::get_writer()
+{
+  if (m_writer)
+    return (0);
+  else
+    return (new LFRingBufferWriter<BlockType>(*this));
+}
+
+template <class BlockType>
+LFRingBufferReader<BlockType>   *LFRingbuffer::get_reader()
+{
+  if (m_reader)
+    return (0);
+  else
+    return (new LFRingBufferReader<BlockType>(*this));
+}
+
+template <class BlockType>
+void            LFRingbuffer::reader_deleted()
+{
+  m_reader = false;
+}
+
+template <class BlockType>
+void            LFRingbuffer::writer_deleted()
+{
+  m_writer = false;
+}
+
+template <class BlockType>
+bool            LFRingbuffer::write(BlockType *out)
+{
+  bool          ret = ready_to_write();
+  if (ret)
+    {
+      m_buffer[m_wo] = *out;
+      m_wo++;
+      m_wo %= m_capacity;
+      m_size--;
+    }
+  return (ret);
+}
+
+template <class BlockType>
+bool            LFRingbuffer::read(BlockType *out)
+{
+  bool          ret = ready_to_read();
+  if (ret)
+    {
+      *out = m_buffer[m_ro];
+      m_ro++;
+      m_ro %= m_capacity;
+      m_size++;
+    }
+  return (ret);
+}
+
+template <class BlockType>
+bool            LFRingbuffer::ready_to_write()
+{
+  return (m_size < m_capacity);
+}
+
+template <class BlockType>
+bool            LFRingbuffer::ready_to_read()
+{
+  return (!(m_size > 0));
+}
+
+// Reader
+
+template <class BlockType>
+LFRingBufferReader::LFRingBufferReader(LFRingBuffer<BlockType> &ringbuf)
+  : ring(ringbuf)
+{
+}
+
+template <class BlockType>
+LFRingBufferReader::~LFRingBufferReader()
+{
+  ring.reader_deleted();
+}
+
+template <class BlockType>
+bool            LFRingBufferReader::read(BlockType *out)
+{
+  return (ring.read(out));
+}
+
+template <class BlockType>
+bool            LFRingBufferReader::ready()
+{
+  return (ring.ready_to_read());
+}
+
+template <class BlockType>
+LFRingBufferWriter::LFRingBufferWriter(LFRingBuffer<BlockType> &ringbuf)
+  : ring(ringbuf)
+{
+}
+
+template <class BlockType>
+LFRingBufferWriter::~LFRingBufferWriter()
+{
+  ring.writer_deleted();
+}
+
+template <class BlockType>
+bool            LFRingBufferWriter::read(BlockType *out)
+{
+  return (ring.write(out));
+}
+
+template <class BlockType>
+bool            LFRingBufferWriter::ready()
+{
+  return (ring.ready_to_write());
 }
