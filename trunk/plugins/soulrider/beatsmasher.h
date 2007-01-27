fb5bf5d914ac9c3a2f10c182780c580ec141c36e
goctaf <goctaf@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-01-27 05:00:55 +0000

    soulrider set_pitch

--- trunk/plugins/soulrider/beatsmasher.h
+++ trunk/plugins/soulrider/beatsmasher.h
@@ -19,6 +19,14 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 01222-1307  USA
 
 ****************************************************************************/
+//
+// Class: BitSmasher
+//
+// Created by: GESTES Cedric <goctaf@gmail.com>
+// Created on: Sun Dec  3 19:34:17 2006
+//
+#ifndef _BITSMASHER_H_
+#define _BITSMASHER_H_
 
 class BitSmasher {
 	public:
@@ -26,28 +34,43 @@ class BitSmasher {
 			m_samplerate = samplerate;
 			m_buffer = new float[samplerate];
 			m_active = false;
+			m_loop_size = 0;
+			m_pos = 0;
 		}
-		
+/*
+ * name: inconnu
+ * @param
+ * @return
+ */
+
+/***
+ *
+ ***/
 		void process(float *buffer, int samplecount) {
 			int i = 0, j = 0;
 			if (!m_active)
 				return;
+			while (j < samplecount)
 			if (m_loadbuffer) {
-				for (m_pos; m_pos < m_loop_size && j < samplecount; ++m_pos) {
+				int ret = samplecount < m_loop_size - m_pos ? m_loop_size - m_pos : samplecount;
+				for (m_pos; m_pos < ret; ++m_pos) {
 					m_buffer[m_pos] = buffer[j];
 					++j;
 				}
+			for (m_pos; m_pos < m_loop_size; ++m_pos) {
 			}
-			
-			
-			
-			
-			
+
+
+
+
+
 		}
-		
-		inline void active(bool active) { 
+
+		inline void active(bool active) {
 			m_active = active;
-			if (m_active = false) {
+			if (m_active) {
+				m_pos = 0;
+				m_loadbuffer = true;
 			}
 		}
 
@@ -59,6 +82,7 @@ class BitSmasher {
 		bool m_loadbuffer;
 		bool m_active;
 		bool m_sync;
-		
+
 };
 
+#endif
