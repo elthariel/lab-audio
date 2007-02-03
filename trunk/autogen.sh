d7bce6055e940c2ebe2d8e70f534400bb563f7a7
goctaf <goctaf@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-02-03 04:52:53 +0000

    root makefile

---
+++ trunk/autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+#!/bin/sh
+
+top=`pwd`
+
+echo 'Generating necessary files (recursively)...'
+
+aclocal &
+
+autogen_dir() {
+	dir=$1
+	name=$2
+	echo -ne $name
+	cd $dir
+	./autogen.sh
+	cd ..
+}
+
+autogen_dir libs/libthc "* libthc * \t"
+
+automake --foreign --add-missing
+autoconf
