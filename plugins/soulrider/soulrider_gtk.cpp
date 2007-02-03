77154d275f275c5077593f8379d63f733207a0f3
goctaf <goctaf@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-02-03 03:51:13 +0000

    new soundmap class

--- plugins/soulrider/soulrider_gtk.cpp
+++ plugins/soulrider/soulrider_gtk.cpp
@@ -1,3 +1,24 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+//
+// Class: soulrider
+//
+// Created by: GESTES Cedric <ctaf42@gmail.com>
+// Created on: Mon Jan  29 19:34:17 2007
+//
 #include <iostream>
 
 #include <gtkmm.h>
@@ -71,6 +92,7 @@ public:
  		bind_param(m_scale_beatsmash, peg_beatsmasher_length);
  		m_scale_pos.signal_value_changed().connect(sigc::bind(sigc::mem_fun(*this, &MyPluginGUI::send_noteonoff), 64));
  		m_scale_pos.set_update_policy(Gtk::UPDATE_DELAYED);//Gtk::UPDATE_DISCONTINUOUS
+ 		m_scale.set_value(1.0);
   }
 
 	void dropable_btn(Gtk::Button &btn, int port) {
