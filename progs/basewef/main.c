214236194ab43e5b33c9737b65493c70c6aaa11c
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-02-10 08:50:06 +0000

    New Wefs

--- progs/basewef/main.c
+++ progs/basewef/main.c
@@ -14,9 +14,11 @@ int main(int argc, char *argv[])
 {
 	wefmaker("wefs/sin.wef", "mySine - 4096\n",
 				"Elthariel", &sine_gen);
-	wefmaker("wefs/saw.wef", "mySaw - trivial generation - 4096]n",
+	wefmaker("wefs/saw.wef", "mySaw - trivial generation - 4096n",
 				"Elthariel", &saw_gen);
 	wefmaker("wefs/square.wef", "mySquare - trivial generation - 1024\n",
 				"Elthariel", &square_gen);
+  wefmaker("wefs/exp.wef", "Exp base e", "Elthariel", &exp_gen);
+  wefmaker("wefs/exp10.wef", "Exp base 10", "Elthariel", &exp10_gen);
 	return 0;
 }
