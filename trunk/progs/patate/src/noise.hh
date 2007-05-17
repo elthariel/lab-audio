8566c374cc8518508ced160f502acbf0e3f13a8e
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-05-17 15:43:12 +0000

    patate : noise generators

--- trunk/progs/patate/src/noise.hh
+++ trunk/progs/patate/src/noise.hh
@@ -23,10 +23,14 @@
 #ifndef   	NOISE_HH_
 # define   	NOISE_HH_
 
-class iWhiteNoise
+# include "dsp_config.h"
+
+#define PI 3.1415926536f
+
+class iRand
 {
 public:
-  virtual ~iWhiteNoise();
+  virtual ~iRand(){}
 
   virtual void          seed(char *a_seed, unsigned int a_len) = 0;
   virtual uint32_t      make_uint32() = 0;
@@ -37,13 +41,90 @@ public:
 private:
 };
 
-class URandom : public iWhiteNoise
+class URandom : public iRand
 {
+public:
+  URandom(const char *path = "/dev/urandom");
+  ~URandom(){}
   void          seed(char *a_seed, unsigned int a_len);
   uint32_t      make_uint32();
   int32_t       make_int32();
   float         make_float();
   double        make_double();
+protected:
+  static int    m_fd;
+  static bool   m_write;
+
 };
+
+class iOsc
+{
+public:
+  iOsc();
+  virtual ~iOsc(){}
+
+  virtual void          reset() = 0;
+  virtual float         frequency();
+  virtual void          frequency(float new_frew);
+  virtual unsigned int  &sampler_rate();
+  virtual sample_t      sample() = 0;
+protected:
+  float                 m_freq;
+  unsigned int          m_sample_rate;
+};
+
+class iOscVector
+{
+public:
+  iOscVector();
+  virtual ~iOscVector(){}
+
+  virtual void          reset() = 0;
+  virtual float         frequency();
+  virtual void          frequency(float new_frew);
+  virtual unsigned int  &sampler_rate();
+  virtual void          render(sample_t *out, unsigned int out_len) = 0;
+
+protected:
+  float                 m_freq;
+  unsigned int          m_sample_rate;
+};
+
+class iOscVectorAdapter : public iOscVector
+{
+public:
+  iOscVectorAdapter(iOsc &a_osc);
+  ~iOscVectorAdapter();
+
+  virtual void          reset() = 0;
+  virtual void          render(sample_t *out, unsigned int out_len) = 0;
+protected:
+  iOsc                  &m_osc;
+};
+
+class WhiteNoise : public iOsc
+{
+public:
+  WhiteNoise(iRand &a_rand);
+  virtual void          reset();
+  virtual sample_t      sample();
+protected:
+  iRand                 &m_rand;
+};
+
+class GaussianWhiteNoise : public iOsc
+{
+public:
+  GaussianWhiteNoise(iRand &a_rand);
+  virtual void          reset();
+  virtual sample_t      sample();
+protected:
+  iRand                 &m_rand;
+};
+
+
+
+
+
 
 #endif	    /* !NOISE_HH_ */
