4c36baa167d82757d519ee1bf3fed7630b7019b7
elthariel <elthariel@1af88c7a-b823-0410-a2b1-7b2def1caa3e>
2007-11-22 06:31:35 +0000

    ttl file for ressource sharing

--- extensions/restype/lv2-restype.h
+++ extensions/restype/lv2-restype.h
@@ -42,10 +42,15 @@
 /// @see LV2_SAMPLE_INTER
 #define                         LV2_RES_TYPE_SAMPLE     1
 
+/*! \brief LV2_RESSOURCE exntesion port type.  A port of the type
+** <http://audio.osslab.eu/lv2/ext/RessourcePort> should point to an
+** instance of this type.
+**
+*/
 typedef struct                  _LV2_RESSOURCE
 {
-  uint32_t                      type;
-  void                          *data;
+  uint32_t                      type; /// must be set to the type of ressource @see LV2_RES_TYPE_SAMPLE
+  void                          *data; /// The actual ressource.
 }                               LV2_RESSOURCE;
 
 /*! \brief This data structure is used to hold a mono audio sample
@@ -53,14 +58,25 @@ typedef struct                  _LV2_RESSOURCE
 **
 ** Channels are interleaved, and values are normalized between -1.0
 ** and 1.0
+**
+** RessourcePort with this subclass
+** <http://audio.osslab.eu/lv2/ext/RessourcePort#sample> must have the
+** data pointer pointing on an instance of this type.
 */
 typedef struct                  _LV2_SAMPLE
 {
-  uint64_t                      size;
-  uint32_t                      sample_rate;
-  uint32_t                      channels;
+  uint64_t                      size; ///The size of the samples in frames (frame is channels samples long)
+  uint32_t                      sample_rate; /// The sample rate of the sample
+  uint32_t                      channels; /// The number of channels. Channels are iterleaved in the data.
+
+  /*!
+  ** If a sample has 2 channels, for example, the dayout layout is the
+  ** following :
+  **
+  * [sample_0_left | sample_0_right | sample_1_left | sample_1_right | ...]
+  */
   float                         *data;
-}                               LV2_SAMPLE_MONO;
+}                               LV2_SAMPLE;
 
 #endif 	    /* !LV2_RESTYPE_H_ */
 
